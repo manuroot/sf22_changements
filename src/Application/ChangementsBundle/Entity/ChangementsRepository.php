c589d4decc9e908027230e070d7c0e77201f37b5
manuroot <mroot72000@yahoo.fr>
2013-06-14 11:35:48 +0200

    sf23: correction bouton retour changement

--- src/Application/ChangementsBundle/Entity/ChangementsRepository.php
+++ src/Application/ChangementsBundle/Entity/ChangementsRepository.php
@@ -53,46 +53,7 @@ class ChangementsRepository extends EntityRepository implements ProviderInterfac
       $queryBuilder->groupBy('a.title');
       $queryBuilder->orderBy('a.title', 'DESC'); */
 
-    public function myFindbAll() {
-        //$fields = array('d.id', 'd.name', 'o.id');
-        //->select($fields)
-        //  $fields = array('a', 'b.id','b.nomprojet','c','d','f','h');
-//$fields = 'partial d.{id, name}, partial o.{id}';  //if you want to get entity object
-        /* $query = $em->createQuery('SELECT DISTINCT c0_.id AS id0, c0_.nom AS nom1, c0_.date_debut AS date_debut2, c0_.date_fin AS date_fin3, c1_.nomprojet AS nomprojet4, c2_.nom_user AS nom_user5, GROUP_CONCAT( DISTINCT c3_.nom_user ) AS sclr6, GROUP_CONCAT( DISTINCT e4_.nom ) AS sclr7
-          FROM changements c0_
-          LEFT JOIN certificats_projet c1_ ON c0_.id_projet = c1_.id
-          LEFT JOIN chrono_user c2_ ON c0_.demandeur = c2_.id
-          INNER JOIN changements_users c5_ ON c0_.id = c5_.changements_id
-          LEFT JOIN chrono_user c3_ ON c3_.id = c5_.chronouser_id
-          LEFT JOIN changements_environnements c6_ ON c0_.id = c6_.changements_id
-          LEFT JOIN environnement e4_ ON e4_.id = c6_.environnements_id
-          GROUP BY c0_.id
-          ORDER BY `c1_`.`nomprojet` ASC');
-
-          $em = $this->getDoctrine()->getManager();
-          $qb = $em->createQueryBuilder();
-          $result = $qb->select('n.user1lname')->from('MySite\MyBundle\Entity\User', 'n')
-          ->where($qb->expr()->like('n.user1lname', $qb->expr()->literal($searchTerm.'%')))
-          ->getQuery()
-          ->getResult();
-         */
-        return $this->createQueryBuilder('a')
-                        //  ->select($fields)
-                        //GROUP_CONCAT( DISTINCT c3_.nom_user ) 
-                        ->select(array('a,b,c,d,f,h'))
-                        ->leftJoin('a.idProjet', 'b')
-                        ->leftJoin('a.demandeur', 'c')
-                        ->leftJoin('a.idStatus', 'd')
-                        ->leftJoin('a.idusers', 'e')
-                        ->leftJoin('a.picture', 'f')
-                        ->leftJoin('a.idEnvironnement', 'g')
-                        ->leftJoin('a.comments', 'h')
-                        ->groupby('a.nom')
-                        ->add('orderBy', 'a.id DESC');
-        // ->getQuery();
-        //   ->leftJoin('a.demandeur', 'c')
-        //  ->getQuery();
-    }
+   
 
     public function getMyPager(array $criteria, $ret = 'getquery') {
 
@@ -156,48 +117,9 @@ class ChangementsRepository extends EntityRepository implements ProviderInterfac
             return $query->getQuery();
         //return $query->getQuery()->getResult();
     }
-public function myFindtstAll($criteria=array()) {
-         $parameters = array();
-         $query = $this->createQueryBuilder('a')
-                ->select(array('a,b,c,d,f,h'))
-                         ->leftJoin('a.idProjet', 'b')
-                        ->leftJoin('a.demandeur', 'c')
-                        ->leftJoin('a.idStatus', 'd')
-                      ->leftJoin('a.idusers', 'e')
-                        ->leftJoin('a.picture', 'f')
-                ->leftJoin('a.idEnvironnement','g')
-                  ->leftJoin('a.comments','h');
-       $query->groupby('a.nom')
-               ->add('orderBy', 'a.id DESC');
-    
-       return $query;
-        
-    }
-    
-     public function myFindnnAll($criteria = array()) {
 
-       $parameters = array();
-       $values=array('a,partial b.{id,nomprojet},partial c.{id,nomUser},partial d.{id,nom,description},f,partial h.{id}');
-       $query = $this->createQueryBuilder('a')
-                ->select($values)
-                ->leftJoin('a.idProjet', 'b')
-                ->leftJoin('a.demandeur', 'c')
-                ->leftJoin('a.idStatus', 'd')
-                  ->leftJoin('a.picture', 'f')
-               
-                ->addSelect('g')
-                ->distinct('GroupConcat(nom)')
-                ->leftJoin('a.idEnvironnement', 'g')
-                 ->leftJoin('a.comments', 'h')
-                ->addSelect('e')
-          
-              ->distinct('GroupConcat(nomUser)')
-                ->leftJoin('a.idusers', 'e');
-                  $query->add('orderBy', 'a.id DESC');
-        return $query;
-        //->getQuery();
-    }
-
+    
+    
      public function myFindAll($criteria = array()) {
 
            $parameters = array();
@@ -224,30 +146,7 @@ public function myFindtstAll($criteria=array()) {
         //->getQuery();
     }
 
-     public function myFind11All($criteria = array()) {
-
-            $parameters = array();
-        $values=array('a,partial b.{id,nomprojet},partial c.{id,nomUser},partial d.{id,nom,description},f,partial h.{id}');
-       $query = $this->createQueryBuilder('a')
-                ->select($values)
-                ->leftJoin('a.idProjet', 'b')
-                ->leftJoin('a.demandeur', 'c')
-                ->leftJoin('a.idStatus', 'd')
-                 ->leftJoin('a.picture', 'f')
-                 ->addSelect('g')
-                ->distinct('GroupConcat(nom)')
-                ->leftJoin('a.idEnvironnement', 'g')
-                  ->leftJoin('a.comments', 'h')
-                ->addSelect('e')
-                  ->distinct('GroupConcat(nomUser)')
-                ->leftJoin('a.idusers', 'e')
-       
-               ;
-                  $query->add('orderBy', 'a.id DESC');
-        return $query;
-  
-    }
-
+   
       public function myFindsimpleAll($criteria = array()) {
 
         //$em = $this->getDoctrine()->getManager();
@@ -272,138 +171,7 @@ public function myFindtstAll($criteria=array()) {
      return $query;
       }
       
-    public function myFindoldAll($criteria = array()) {
-
-         $parameters = array();
-   //           $values=array('a,partial b.{id,nomprojet},partial c.{id,nomUser},partial d.{id,nom,description},f,partial h.{id}');
    
-        $values=array('a,partial b.{id,nomprojet},partial c.{id,nomUser},partial d.{id,nom,description},f,partial h.{id}');
-         //$values=array('a,partial b.{id,nomprojet},partial c.{id,nomUser},partial d.{id,nom,description},f,partial h.{id},GroupConcat(i.nomUser)');
-            
-       $query = $this->createQueryBuilder('a')
-                   ->select($values)
-                ->leftJoin('a.idProjet', 'b')
-                ->leftJoin('a.demandeur', 'c')
-                ->leftJoin('a.idStatus', 'd')
-                 ->leftJoin('a.picture', 'f')
-                ->addSelect('g')
-                ->distinct('GroupConcat(nom)')
-                ->leftJoin('a.idEnvironnement', 'g')
-               ->leftJoin('a.comments', 'h')
-                ->addSelect('e')
-     
-              ->distinct('GroupConcat(nomUser)')
-                ->leftJoin('a.idusers', 'e')
-               
-              //  ->addselect('GroupConcat(e.nomUser)');;
-        //   $query->setParameters($parameters);
-        // ??
-               ;
-      // $query->addSelect('distinct i.nomUser');
-      //  $query->groupby('a.id');
-               $query->add('orderBy', 'a.id DESC');
-        return $query;
-        //->getQuery();
-    }
-
-    /* $em = $this->getManager();
-      $query = $em->createQuery('SELECT DISTINCT c0_.id AS id0, c0_.nom AS nom1,
-     * c0_.date_debut AS date_debut2, c0_.date_fin AS date_fin3, c1_.nomprojet AS nomprojet4,
-     * c2_.nom_user AS nom_user5, GROUP_CONCAT( DISTINCT c3_.nom_user ) AS sclr6, GROUP_CONCAT( DISTINCT e4_.nom ) AS sclr7
-      FROM changements c0_
-      LEFT JOIN certificats_projet c1_ ON c0_.id_projet = c1_.id
-      LEFT JOIN chrono_user c2_ ON c0_.demandeur = c2_.id
-      INNER JOIN changements_users c5_ ON c0_.id = c5_.changements_id
-      LEFT JOIN chrono_user c3_ ON c3_.id = c5_.chronouser_id
-      LEFT JOIN changements_environnements c6_ ON c0_.id = c6_.changements_id
-      LEFT JOIN environnement e4_ ON e4_.id = c6_.environnements_id
-      GROUP BY c0_.id
-      ORDER BY `c1_`.`nomprojet` ASC');
-      return $query->getResult(); */
-
-    //$fields = array('d.id', 'd.name', 'o.id');
-    //->select($fields)
-    //  $fields = array('a', 'b.id','b.nomprojet','c','d','f','h');
-//$fields = 'partial d.{id, name}, partial o.{id}';  //if you want to get entity object
-    /*
-     * 
-     * ->where('det.id IN (:miarray)')
-      ->setParameter('miarray', array('143','144'))
-     */
-    /*   $parameters = array();
-      $query = $this->createQueryBuilder('a')
-      //return $this->createQueryBuilder('a')
-      //  ->select($fields)
-      ->select(array('a,b,c,d,f,g,h'))
-      //  ->select(array('a,b,c,d,e,f,g,h'))
-      ->leftJoin('a.idProjet', 'b')
-      ->leftJoin('a.demandeur', 'c')
-      ->leftJoin('a.idStatus', 'd')
-      // ->addSelect(GroupConcat(e.nomUser, ' / '))
-      ->leftJoin('a.idusers', 'e')
-      ->leftJoin('a.picture', 'f')
-      ->leftJoin('a.idEnvironnement','g')
-      // ->where('g.id = :changement_id')
-      // ->setParameter('changement_id', 3)
-      ->leftJoin('a.comments','h');
-      //   $query->setParameters($parameters);
-      // ??
-      $query->groupby('a.id')
-      ->add('orderBy', 'a.id DESC');
-
-      return $query;
-
-     */
-
-    /*
-      $em = $this->getManager();
-
-      $query = "SELECT DISTINCT
-      a.id AS id0,
-      a.nom AS nom1,
-      a.date_debut AS date_debut2,
-      a.date_fin AS date_fin3,
-      c1_.nomprojet AS nomprojet4,
-      c2_.nom_user AS nom_user5,
-      GROUP_CONCAT(distinct c3_.nom_user) AS sclr6,
-      GROUP_CONCAT(distinct e4_.nom) AS sclr7
-      FROM
-      changements a
-      LEFT JOIN certificats_projet c1_ ON a.id_projet = c1_.id
-      LEFT JOIN chrono_user c2_ ON a.demandeur = c2_.id
-      LEFT JOIN changements_users c5_ ON a.id = c5_.changements_id
-      LEFT JOIN chrono_user c3_ ON c3_.id = c5_.chronouser_id
-      LEFT JOIN changements_environnements c6_ ON a.id = c6_.changements_id
-      LEFT JOIN environnement e4_ ON e4_.id = c6_.environnements_id
-      GROUP BY
-      a.id
-      ORDER BY
-      sclr6 ASC";
-      $rsm = new ResultSetMapping;
-
-      //$rsm->addFieldResult('t', 'id', 'id');
-
-      $qns = $this->_em->createNativeQuery($query, $rsm);
-
-      $qns->getResult();
-      /*
-     * SELECT DISTINCT a,b,c,d,GroupConcat( DISTINCT e.nomUser ),f,h FROM Application\ChangementsBundle\Entity\Changements a 
-      LEFT JOIN a.idProjet b LEFT JOIN a.demandeur c LEFT JOIN a.idStatus d
-      LEFT JOIN a.idusers e LEFT JOIN a.picture f
-      LEFT JOIN a.idEnvironnement g
-      LEFT JOIN a.comments h GROUP BY a.nom ORDER BY a.id DESC');
-     */
-    /*
-      $query = $em->createQuery('SELECT DISTINCT a,b,c,d,f,h FROM Application\ChangementsBundle\Entity\Changements a
-      LEFT JOIN a.idProjet b LEFT JOIN a.demandeur c LEFT JOIN a.idStatus d LEFT JOIN a.idusers e LEFT JOIN a.picture f LEFT JOIN a.idEnvironnement g LEFT JOIN a.comments h GROUP BY a.nom ORDER BY a.id DESC');
-     */
-    // return $qns;
-    //->getQuery();
-    // ->groupby('a.nom')
-    // ->getQuery();
-    //   ->leftJoin('a.demandeur', 'c')
-    //  ->getQuery();
-    //  }
     public function getEventsQueryBuilder(\DateTime $begin, \DateTime $end, array $options = array()) {
         $qb = $this->createQueryBuilder('e');
 
