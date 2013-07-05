145bb7d52b207a3067cb55bb28e047aa3cd927ba
manuroot <mroot72000@yahoo.fr>
2013-07-05 18:23:55 +0200

    sf23:  fos_js pour ajax routes

---
+++ web/js/compiled/changements_editme_4.js
@@ -0,0 +1,20 @@
+  $(document).ready(function(){
+        //Examples of how to assign the ColorBox event to elements
+        //  href:"/uzf04new/chrono/example",
+        $("a.editme").colorbox({
+            iframe:true,
+            transition:	"elastic",
+            //   photo:true,
+        /*    ajax:true,*/
+            //     html:true,
+            width:"70%",
+            height:"70%",
+           /*  width:"600px",
+            height:"400px",*/
+            //       href:"/uzf04new/chrono/example",
+            fastIframe:false,
+            opacity:0.3
+        
+        });
+ 		
+    });
