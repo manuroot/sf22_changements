bbd32c71bd2e1d00a303e6c2c5ad1c7376e0c69c
manuroot <mroot72000@yahoo.fr>
2013-07-31 21:59:54 +0200

    sf23:  modif js icons

--- src/Application/ChangementsBundle/Resources/public/js/datenew.js
+++ src/Application/ChangementsBundle/Resources/public/js/datenew.js
@@ -1,66 +1,66 @@
- $(document).ready(function() {
-    
-     //$(function() {
-      $( "#changements_dateDebut" ).datepicker({
-    maxDate: "+5Y",
-    minDate: "-5Y",
-    changeMonth: true,
-    changeYear: true,
-    numberOfMonths: 1,
-    dateFormat: "yy-mm-dd 08:00",
-    onClose: function( selectedDate ) {
-    $( "#form_bis" ).datepicker( "option", "minDate", selectedDate );
-    }
-    });
-        $( "#changements_dateFin" ).datepicker({
-    maxDate: "+5Y",
-    minDate: "-5Y",
-    changeMonth: true,
-    changeYear: true,
-    numberOfMonths: 1,
-     dateFormat: "yy-mm-dd 17:00",
-    onClose: function( selectedDate ) {
-    $( "#form_bis" ).datepicker( "option", "minDate", selectedDate );
-    }
-    });
-      $( "#changements_dateComep" ).datepicker({
-    maxDate: "+5Y",
-    minDate: "-5Y",
-    changeMonth: true,
-    changeYear: true,
-    numberOfMonths: 1,
-    dateFormat: "yy-mm-dd",
-    onClose: function( selectedDate ) {
-    $( "#form_bis" ).datepicker( "option", "minDate", selectedDate );
-    }
-    });
-      $( "#changements_dateVsr" ).datepicker({
-     maxDate: "+5Y",
-    minDate: "-5Y",
-     changeMonth: true,
-    changeYear: true,
-    numberOfMonths: 1,
-    dateFormat: "yy-mm-dd",
-    onClose: function( selectedDate ) {
-    $( "#form_bis" ).datepicker( "option", "minDate", selectedDate );
-    }
-    });
-  /*
-   $('.btn-add').click(function(event) {
-        var collectionHolder = $('#' + $(this).attr('data-target'));
-        var prototype = collectionHolder.attr('data-prototype');
-        var form = prototype.replace(/__name__/g, collectionHolder.children().length);
+$(document).ready(function() {
 
-        collectionHolder.append(form);
-
-        return false;
-    });*/
+    //$(function() {
+    $("#changements_dateDebut").datepicker({
+        minDate: "-5Y",
+        maxDate: "+10Y",
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+        dateFormat: "yy-mm-dd",
+        onClose: function(selectedDate) {
+            $("#form_bis").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#changements_dateFin").datepicker({
+        maxDate: "+5Y",
+        minDate: "-5Y",
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+        dateFormat: "yy-mm-dd 17:00",
+        onClose: function(selectedDate) {
+            $("#form_bis").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#changements_dateComep").datepicker({
+        maxDate: "+5Y",
+        minDate: "-5Y",
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+        dateFormat: "yy-mm-dd",
+        onClose: function(selectedDate) {
+            $("#form_bis").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#changements_dateVsr").datepicker({
+        maxDate: "+5Y",
+        minDate: "-5Y",
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+        dateFormat: "yy-mm-dd",
+        onClose: function(selectedDate) {
+            $("#form_bis").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    /*
+     $('.btn-add').click(function(event) {
+     var collectionHolder = $('#' + $(this).attr('data-target'));
+     var prototype = collectionHolder.attr('data-prototype');
+     var form = prototype.replace(/__name__/g, collectionHolder.children().length);
+     
+     collectionHolder.append(form);
+     
+     return false;
+     });*/
 
     /*$('.btn-remove').live('click', function(event) {
-        var name = $(this).attr('data-related');
-        $('*[data-content="'+name+'"]').remove();
-
-        return false;
-    });
-   */
+     var name = $(this).attr('data-related');
+     $('*[data-content="'+name+'"]').remove();
+     
+     return false;
+     });
+     */
 }); //Eof:: ready
