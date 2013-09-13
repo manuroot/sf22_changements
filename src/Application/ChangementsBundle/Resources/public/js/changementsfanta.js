201649e23d968e71337d445e3613323b206d2f15
manuroot <manuroot@localhost>
2013-09-13 08:28:23 +0200

    Update jquery changements

--- src/Application/ChangementsBundle/Resources/public/js/changementsfanta.js
+++ src/Application/ChangementsBundle/Resources/public/js/changementsfanta.js
@@ -1,73 +1,124 @@
 $(document).ready(function() {
-    var img_path = 'bundles/applicationchangements/images/';
+    var img_path = '/bundles/applicationchangements/images/';
     var img_s_path = 'bundles/applicationchangements/images/';
+
+ /*   function checkdiag() {
+            bootbox.dialog({
+  message: "I am a custom dialog",
+  title: "Custom title",
+  buttons: {
+    success: {
+      label: "Success!",
+      className: "btn-success",
+      callback: function() {
+        Example.show("great success");
+      }
+    },
+    danger: {
+      label: "Danger!",
+      className: "btn-danger",
+      callback: function() {
+        Example.show("uh oh, look out!");
+      }
+    },
+    main: {
+      label: "Click ME!",
+      className: "btn-primary",
+      callback: function() {
+        Example.show("Primary button");
+      }
+    }
+  }
+});
+   
+
+    }
+    bootbox.confirm("Are you sure?", function(result) {
+  
+});*/
     $("td > a").click(function(event) {
 
-  if ($(this).hasClass("favoris")) {
-    var id = $(this).attr("data-id");
-      var name = $(this).attr("data-name");
-      var message="";
-      var new_status=0;
-      var img_favori="";
+        if ($(this).hasClass("favoris")) {
+            var id = $(this).attr("data-id");
+            var name = $(this).attr("data-name");
+            var message = "";
+            var new_status = 0;
+            var img_favori = "";
+            var modal_res = "";
             var status = $(this).attr("data-status");
-  console.log("favoris id=" + id);
-  if (status == 1 ) { 
-      message="Supprimer de "; 
-      img_favori="star-off.png";
-      
-  }
-  else { message="Ajouter a ";    img_favori="star-on.png"; 
-  new_status=1;}
- 
-  var checkstr =  confirm(message + " vos favoris: \nstatus=" + status + "\nid=" + id + "\nnom=" + name);
-            if(checkstr === true){
-                 $(this).children().attr("src", img_path + img_favori);
-                 $(this).attr('data-status',new_status)
-                 /*$(this).data('data-status',new_status);*/
-                 var dataAjax = {id: id};
-            return true;
+            console.log("favoris id=" + id);
+            if (status === 1) {
+                message = "Supprimer de ";
+                img_favori = "star-off.png";
+
             }
-           
-            else{
+
+            else {
+                message = "Ajouter a ";
+                img_favori = "star-on.png";
+                new_status = 1;
+            }
+
+            /*    bootbox.confirm("Are you sure?", function(result) {
+             Example.show("Confirm result: "+result);
+             }); */
+             var checkstr =  confirm(message + " vos favoris: \nstatus=" + status + "\nid=" + id + "\nnom=" + name);
+           /* var checkstr = false;*/
+          /*  bootbox.confirm("Are you sure?", function(result) {*/
+               /* console.log("result=" + result);*/
+              /*  checkstr = result;*/
+      
+            
+           /* console.log("result main=" + checkstr);*/
+            if (checkstr === true) {
+                $(this).children().attr("src", img_path + img_favori);
+                $(this).attr('data-status', new_status);
+                /*$(this).data('data-status',new_status);*/
+                var dataAjax = {id: id};
+                return true;
+            }
+
+            else {
                 return false;
             }
-  }
+            /*});*/
+        }
         /* * A modifier: change color only sur success !!*/
         if ($(this).hasClass("open") || $(this).hasClass("closed") || $(this).hasClass("prepare")) {
-           var id = $(this).attr("data-id");
-           var name = $(this).attr("data-name");
-          var checkstr =  confirm("Modifier le status de la demande: \nid=" + id + "\nnom=" + name);
-            if(checkstr === true){
-            // do your code
-           
-            /*else{
-                return false;
-            }*/
-         /* var id = $(this).attr("data-id");*/
-            console.log("id=" + id);
-            var dataAjax = {id: id};
+            var id = $(this).attr("data-id");
+            var name = $(this).attr("data-name");
+            var checkstr = confirm("Modifier le status de la demande: \nid=" + id + "\nnom=" + name);
+            if (checkstr === true) {
+                // do your code
 
-            if ($(this).hasClass("open")) {
-                $(this).removeClass("open").addClass("closed");
-                $(this).closest("tr").removeClass("success").addClass("myclosed");
-                $(this).children().attr("src", img_path + "cadenas-sferme.png");
+                /*else{
+                 return false;
+                 }*/
+                /* var id = $(this).attr("data-id");*/
+                console.log("id=" + id);
+                var dataAjax = {id: id};
+
+                if ($(this).hasClass("open")) {
+                    $(this).removeClass("open").addClass("closed");
+                    $(this).closest("tr").removeClass("success").addClass("myclosed");
+                    $(this).children().attr("src", img_path + "cadenas-sferme.png");
+                }
+                else if ($(this).hasClass("prepare")) {
+                    $(this).children().attr("src", img_path + "cadenas-souvert.png");
+                    $(this).removeClass("prepare").addClass("open");
+                    $(this).closest("tr").removeClass("prepare").addClass("success");
+                }
+                //cas closed: closed ==> prepare
+                else if ($(this).hasClass("closed")) {
+                    /*   console.log("closed test button");*/
+                    $(this).children().attr("src", img_path + "cadenas-sbleu.png");
+                    $(this).removeClass("closed").addClass("prepare");
+                    $(this).closest("tr").removeClass("myclosed").addClass("prepare");
+                    /*.addClass("prepare");*/
+                }
+                ;
+                remplirSelect(dataAjax);
             }
-            else if ($(this).hasClass("prepare")) {
-                $(this).children().attr("src", img_path + "cadenas-souvert.png");
-                $(this).removeClass("prepare").addClass("open");
-                 $(this).closest("tr").removeClass("prepare").addClass("success");
-            }
-            //cas closed: closed ==> prepare
-            else if ($(this).hasClass("closed")) {
-                /*   console.log("closed test button");*/
-                $(this).children().attr("src", img_path + "cadenas-bleu.png");
-                $(this).removeClass("closed").addClass("prepare");
-                $(this).closest("tr").removeClass("myclosed").addClass("prepare");
-                /*.addClass("prepare");*/
-            }
-            ;
-            remplirSelect(dataAjax);
-        }
         }
     });
     function remplirSelect(dataAjax) {
@@ -90,148 +141,156 @@ $(document).ready(function() {
     } //Eof:: fucntion remplirSelect
 
 
-   // declare freeDays global
-        var eventsDays = [];
-        var eventsTitle = [];
-  
-        function fetchEvents(year, month) {
-            /* console.log("month:" + month);*/
-            var dataAjax = {
-                'year': year,
-                'month': month
-            };
+    // declare freeDays global
+    var eventsDays = [];
+    var eventsTitle = [];
 
-            $.ajax({
-                async: false,
-                url: Routing.generate('changements_minicalendar'),
-                /*url: "{{ path('epost_calendar') }}",*/
-                type: "POST",
-                dataType: "json",
-                data: dataAjax,
-                success: function(reponse) {
-                    var optionData = reponse;
-                    // loop over dayUsage array result
-                    $.each(optionData, function(index, value) {
-                        /*   console.log("added value=" + value.date);*/
-                        /*  eventsDays.push(value.date,value.title); */
-                        eventsDays.push(value.date);
-                        eventsTitle.push(value.title + ' (' + value.date + ')');
-                        // add this date to the freeDays array
-                    });
-                    /* highlightDays(date);*/
-                    /* $('#datepicker').trigger('click');*/
-                }
-            });
-        }
+    function fetchEvents(year, month) {
+        /* console.log("month:" + month);*/
+        var dataAjax = {
+            'year': year,
+            'month': month
+        };
 
-
-
-        function dateToYMD(date) {
-            var d = date.getDate();
-            var m = date.getMonth() + 1;
-            var y = date.getFullYear();
-            return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-        }
-        /*var eventsDays = ['05/01/2013', '05/11/2013'];*/
-        fetchEvents();
-
-        function pickDate(dateStr) {
-            // Do something with the chosen date...
-            window.location.href = links[dateStr];
-        }
-        /*
-         $('.selector').datepicker({
-         onSelect: function(dateText, inst) {
-         
-         window.location.href = '/go-to-page?date='+dateText;
-         }
-         }); */
-        $("#minidatepicker").datepicker({
-            changeMonth: true,
-            changeYear: true,
-            showOtherMonths: true,
-            selectOtherMonths: true,
-            //numberOfMonths: 2,
-            dateFormat: 'yy-mm-dd',
-            /*altField: '#date_due',
-            altFormat: 'yy-mm-dd',*/
-             showWeek: true,
-        firstDay: 1,
-        
-            
-            beforeShowDay: editDays,
-           /* onSelect: function(dateText, inst) {
-                var url = Routing.generate('epost_index');
-                 window.location.href = url + '/' + dateText;
-    
-            },*/
-            onChangeMonthYear: fetchEvents
-            /* create: function(event, ui) {
-             fetchEvents()
-             },*/
-            /*  onLoad:fetchEvents,*/
-           
-        });
-
-        function editDays(date) {
-            /* console.log("edit days");*/
-            for (var i = 0; i < eventsDays.length; i++) {
-                /* var t=new Date(eventsDays[i]).toString("mm-dd-yy");*/
-                var t = new Date(eventsDays[i]).toString();
-                /* console.log("events=" + eventsDays[i] + "--" + t + " date=" + date.toString() + " i=" + i);
-                 */
-                /*    if (t == date.toString()) {     */
-                if (new Date(eventsDays[i]).toString() === date.toString()) {
-                    return [true, 'free-day', eventsTitle[i]];
-                }
+        $.ajax({
+            async: false,
+            url: Routing.generate('changements_minicalendar'),
+            /*url: "{{ path('epost_calendar') }}",*/
+            type: "POST",
+            dataType: "json",
+            data: dataAjax,
+            success: function(reponse) {
+                var optionData = reponse;
+                // loop over dayUsage array result
+                $.each(optionData, function(index, value) {
+                    /*   console.log("added value=" + value.date);*/
+                    /*  eventsDays.push(value.date,value.title); */
+                    eventsDays.push(value.date);
+                    eventsTitle.push(value.title + ' (' + value.date + ')');
+                    // add this date to the freeDays array
+                });
+                /* highlightDays(date);*/
+                /* $('#datepicker').trigger('click');*/
             }
-            return [true];
-        }
-        
-          function format(state) {
-            if (!state.id) return state.text; // optgroup
-            return "<img class='flag' src='" + img_s_path + state.id.toLowerCase() + ".png'/> " + state.text;
-        }
-        $("#changements_searchfilter_idStatus").select2({
-             placeholder: "-- Choisir Statut(s) --",
-                allowClear: true,
-            formatResult: format,
-            formatSelection: format,
-            escapeMarkup: function(m) { return m; }
         });
-        $("#changements_searchfilter_idStatus_cl").click(function() { $("#changements_searchfilter_idStatus").select2("val", ""); });
-        /*
-         $("#changements_searchfilter_idStatus").on("change", function() { $("##changements_searchfilter_idStatus_val").html($("#changements_searchfilter_idStatus").val());});
- 
-$("#changements_searchfilter_idStatus").select2("container").find("ul.select2-choices").sortable({
-containment: 'parent',
-start: function() { $("#changements_searchfilter_idStatus").select2("onSortStart"); },
-update: function() { $("#changements_searchfilter_idStatus").select2("onSortEnd"); }
-});*/
+    }
 
 
 
+    function dateToYMD(date) {
+        var d = date.getDate();
+        var m = date.getMonth() + 1;
+        var y = date.getFullYear();
+        return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+    }
+    /*var eventsDays = ['05/01/2013', '05/11/2013'];*/
+    fetchEvents();
 
-        $("#changements_searchfilter_idProjet").select2({
-                placeholder: "-- Choisir Projet(s) --",
-                allowClear: true
-            });
-        $("#changements_searchfilter_idProjet_cl").click(function() { $("#changements_searchfilter_idProjet").select2("val", ""); });
-      
-        
-        $("#changements_searchfilter_idEnvironnement").select2({
-                placeholder: "-- Choisir Environnement(s) --",
-                allowClear: true
-            });
-              $("#changements_searchfilter_idEnvironnement_cl").click(function() { $("#changements_searchfilter_idEnvironnement").select2("val", ""); });
-      
-      
-             $("#changements_searchfilter_idusers").select2({
-                placeholder: "-- Choisir User(s) --",
-                allowClear: true
-            });
-              $("#changements_searchfilter_idusers_cl").click(function() { $("#changements_searchfilter_idusers").select2("val", ""); });
-      
-         $("#changements_searchfilter_demandeur").select2(); 
+    function pickDate(dateStr) {
+        // Do something with the chosen date...
+        window.location.href = links[dateStr];
+    }
+    /*
+     $('.selector').datepicker({
+     onSelect: function(dateText, inst) {
+     
+     window.location.href = '/go-to-page?date='+dateText;
+     }
+     }); */
+    $("#minidatepicker").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        //numberOfMonths: 2,
+        dateFormat: 'yy-mm-dd',
+        /*altField: '#date_due',
+         altFormat: 'yy-mm-dd',*/
+        showWeek: true, firstDay: 1,
+        beforeShowDay: editDays,
+        /* onSelect: function(dateText, inst) {
+         var url = Routing.generate('epost_index');
+         window.location.href = url + '/' + dateText;
          
+         },*/
+        onChangeMonthYear: fetchEvents
+                /* create: function(event, ui) {
+                 fetchEvents()
+                 },*/
+                /*  onLoad:fetchEvents,*/
+
+    });
+
+    function editDays(date) {
+        /* console.log("edit days");*/
+        for (var i = 0; i < eventsDays.length; i++) {
+            /* var t=new Date(eventsDays[i]).toString("mm-dd-yy");*/
+            var t = new Date(eventsDays[i]).toString();
+            /* console.log("events=" + eventsDays[i] + "--" + t + " date=" + date.toString() + " i=" + i);
+             */
+            /*    if (t == date.toString()) {     */
+            if (new Date(eventsDays[i]).toString() === date.toString()) {
+                return [true, 'free-day', eventsTitle[i]];
+            }
+        }
+        return [true];
+    }
+
+    function format(state) {
+        if (!state.id)
+            return state.text; // optgroup
+        return "<img class='flag' src='" + img_s_path + state.id.toLowerCase() + ".png'/> " + state.text;
+    }
+    $("#changements_searchfilter_idStatus").select2({
+        placeholder: "-- Choisir Statut(s) --",
+        allowClear: true,
+        formatResult: format,
+        formatSelection: format,
+        escapeMarkup: function(m) {
+            return m;
+        }
+    });
+    $("#changements_searchfilter_idStatus_cl").click(function() {
+        $("#changements_searchfilter_idStatus").select2("val", "");
+    });
+    /*
+     $("#changements_searchfilter_idStatus").on("change", function() { $("##changements_searchfilter_idStatus_val").html($("#changements_searchfilter_idStatus").val());});
+     
+     $("#changements_searchfilter_idStatus").select2("container").find("ul.select2-choices").sortable({
+     containment: 'parent',
+     start: function() { $("#changements_searchfilter_idStatus").select2("onSortStart"); },
+     update: function() { $("#changements_searchfilter_idStatus").select2("onSortEnd"); }
+     });*/
+
+
+
+
+    $("#changements_searchfilter_idProjet").select2({
+        placeholder: "-- Choisir Projet(s) --",
+        allowClear: true
+    });
+    $("#changements_searchfilter_idProjet_cl").click(function() {
+        $("#changements_searchfilter_idProjet").select2("val", "");
+    });
+
+
+    $("#changements_searchfilter_idEnvironnement").select2({
+        placeholder: "-- Choisir Environnement(s) --",
+        allowClear: true
+    });
+    $("#changements_searchfilter_idEnvironnement_cl").click(function() {
+        $("#changements_searchfilter_idEnvironnement").select2("val", "");
+    });
+
+
+    $("#changements_searchfilter_idusers").select2({
+        placeholder: "-- Choisir User(s) --",
+        allowClear: true
+    });
+    $("#changements_searchfilter_idusers_cl").click(function() {
+        $("#changements_searchfilter_idusers").select2("val", "");
+    });
+
+    $("#changements_searchfilter_demandeur").select2();
+
 }); //Eof:: ready
