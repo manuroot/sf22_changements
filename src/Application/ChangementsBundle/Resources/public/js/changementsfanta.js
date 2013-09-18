4887a7f1a1ea3bad8d7dba8aced7178f1c142f50
manuroot <mroot72000@yahoo.fr>
2013-09-18 17:27:48 +0200

    correction jquery form fanta

--- src/Application/ChangementsBundle/Resources/public/js/changementsfanta.js
+++ src/Application/ChangementsBundle/Resources/public/js/changementsfanta.js
@@ -359,19 +359,25 @@ $(document).ready(function() {
     });
 
   function postmonth(date) {
+      
+      alert('Form is submitting');
+      //$("#changements_searchfilter_dateDebut").html(date.toString());
+      $("input#changements_searchfilter_dateDebut").val(date);
+      $(".form").submit();
+  
   /*var dataAjax = {
             'year': year,
             'month': month
         };*/
 
-        $.ajax({
+       /* $.ajax({
             async: false,
             url: Routing.generate('changements_fanta'),
-            /*url: "{{ path('epost_calendar') }}",*/
+            
             type: "POST",
             dataType: "json",
             data: date
-         });
+         });*/
          }
         
     function editDays(date) {
@@ -462,5 +468,73 @@ $(document).ready(function() {
     });
 
     $("#changements_searchfilter_demandeur").select2();
-
+/*
+ //if submit button is clicked
+    $('#submit').click(function () {        
+        
+        //Get the data from all the fields
+        var name = $('input[name=name]');
+        var email = $('input[name=email]');
+        var website = $('input[name=website]');
+        var comment = $('textarea[name=comment]');
+        //Simple validation to make sure user entered something
+        //If error found, add hightlight class to the text field
+        if (name.val()=='') {
+            name.addClass('hightlight');
+            return false;
+        } else name.removeClass('hightlight');
+        
+        if (email.val()=='') {
+            email.addClass('hightlight');
+            return false;
+        } else email.removeClass('hightlight');
+        
+        if (comment.val()=='') {
+            comment.addClass('hightlight');
+            return false;
+        } else comment.removeClass('hightlight');
+        
+        //organize the data properly
+        var data = 'name=' + name.val() + '&email=' + email.val() + '&website='
+        + website.val() + '&comment=' + encodeURIComponent(comment.val());
+        
+        //disabled all the text fields
+        $('.text').attr('disabled','true');
+        
+        //show the loading sign
+        $('.loading').show();
+        
+        //start the ajax
+        $.ajax({
+            //this is the php file that processes the data and send mail
+            url: "process.php",    
+            
+            //GET method is used
+            type: "GET",
+            //pass the data            
+            data: data,        
+            
+            //Do not cache the page
+            cache: false,
+            
+            //success
+            success: function (html) {                
+                //if process.php returned 1/true (send mail success)
+                if (html==1) {                    
+                    //hide the form
+                    $('.form').fadeOut('slow');                    
+                    
+                    //show the success message
+                    $('.done').fadeIn('slow');
+                    
+                //if process.php returned 0/false (send mail failed)
+                } else alert('Sorry, unexpected error. Please try again later.');                
+            }        
+        });
+        
+        //cancel the submit button default behaviours
+        return false;
+    });     
+ 
+ */
 }); //Eof:: ready
