f0f280dbd79c48ae7bd5e0ffd7758610fbe7b117
manuroot <manuroot@localhost>
2013-09-30 18:30:10 +0200

    js validate

--- clearinstall_dev.sh
+++ clearinstall_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 php app/console cache:clear --env=dev --no-debug
 php app/console assets:install web --env=dev
-php app/console assetic:dump --env=dev
+php app/console assetic:dump --env=dev 
 chown -R www-data:manuroot ../Symfony_changements
 
