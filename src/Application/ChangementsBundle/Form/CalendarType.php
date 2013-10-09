48660c9d6eeadfd4de4bb051aed05428e0c44731
manuroot <manuroot@localhost>
2013-10-09 20:30:54 +0200

    update calendar

--- src/Application/ChangementsBundle/Form/CalendarType.php
+++ src/Application/ChangementsBundle/Form/CalendarType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManager;
-
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormError;
@@ -19,56 +18,56 @@ use Application\RelationsBundle\Entity\Projet;
 use Application\RelationsBundle\Repository\ProjetRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
-
 class CalendarType extends AbstractType {
 
     private $_em;
-     // datas pour $em
+    // datas pour $em
     private $_datas;
 
-    public function __construct(EntityManager $entityManager,$datas=array()) {
+    public function __construct(EntityManager $entityManager, $datas = array()) {
         $this->_em = $entityManager;
-      //   $this->_datas = $datas;
-         //Array ( [nom] => [description] => [demandeur] => 
-         //[dateDebut] => [dateDebut_max] => [dateFin] => [dateFin_max] => [ticketExt] => [ticketInt] => [idEnvironnement] => Array ( [0] => 1 )
+        //   $this->_datas = $datas;
+        //Array ( [nom] => [description] => [demandeur] => 
+        //[dateDebut] => [dateDebut_max] => [dateFin] => [dateFin_max] => [ticketExt] => [ticketInt] => [idEnvironnement] => Array ( [0] => 1 )
         // $em = $this->getDoctrine()->getManager();
     }
+
     public function buildForm(FormBuilderInterface $builder, array $options) {
 
         $em = $this->_em;
-        $choices_years = $em->getRepository('ApplicationChangementsBundle:Changements')-> GetYears();
-          
+        $choices_years = $em->getRepository('ApplicationChangementsBundle:Changements')->GetYears();
+
         $builder->setAttribute('show_legend', false); // no legend for main form
-       /*
-     
-    }
-        */
-       
-        $min_year=Date('Y')-3;
-        $max_year=Date('Y')+3;
-$builder
-   ->add('publishedAt', 'date', array(
-                            'widget' => 'choice',
-                            'format' => 'yyyy-MMMM-dd',
-                            'pattern' => '{{ year }}-{{ month }}-{{ day }}',
-                            'years' => range($min_year,$max_year),
-        'years' => $choices_years,
-                            'label' => false,
-                            'input' => 'string',
-       'mapped'=>false
-                  
-                        ))
-        ->add('idStatus', 'filter_entity', array(
+        /*
+
+          }
+         */
+
+        $min_year = Date('Y') - 3;
+        $max_year = Date('Y') + 3;
+        $builder
+                ->add('publishedAt', 'date', array(
+                    'widget' => 'choice',
+                    'format' => 'yyyy-MMMM-dd',
+                    'pattern' => '{{ year }}-{{ month }}-{{ day }}',
+                    'years' => range($min_year, $max_year),
+                    'years' => $choices_years,
+                    'label' => false,
+                    'input' => 'string',
+                    'mapped' => false
+                ))
+                ->add('idStatus', 'filter_entity', array(
+                    'attr' => array(
+                        'style' => 'height:30px'),
                     'label' => 'Status',
                     'class' => 'Application\ChangementsBundle\Entity\ChangementsStatus',
                     'property' => 'nom',
                     'expanded' => false,
                     'multiple' => false,
                     'required' => false,
-                     'empty_value' => '--- Choisir une option ---', 
+                    'empty_value' => '- Choisir une option -',
                 ))
-            ;
-         
+        ;
     }
 
     public function getName() {
