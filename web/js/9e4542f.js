95886376c4a15de6c441882e78d0bf9a9c0f2476
root <root@manuroot-linux.(none)>
2014-02-17 08:16:03 +0100

    Adding files

---
+++ web/js/9e4542f.js
@@ -0,0 +1,315 @@
+/*
+ * jQuery Iframe Transport Plugin 1.5
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2011, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint unparam: true, nomen: true */
+/*global define, window, document */
+
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        // Register as an anonymous AMD module:
+        define(['jquery'], factory);
+    } else {
+        // Browser globals:
+        factory(window.jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // Helper variable to create unique names for the transport iframes:
+    var counter = 0;
+
+    // The iframe transport accepts three additional options:
+    // options.fileInput: a jQuery collection of file input fields
+    // options.paramName: the parameter name for the file form data,
+    //  overrides the name property of the file input field(s),
+    //  can be a string or an array of strings.
+    // options.formData: an array of objects with name and value properties,
+    //  equivalent to the return data of .serializeArray(), e.g.:
+    //  [{name: 'a', value: 1}, {name: 'b', value: 2}]
+    $.ajaxTransport('iframe', function (options) {
+        if (options.async && (options.type === 'POST' || options.type === 'GET')) {
+            var form,
+                iframe;
+            return {
+                send: function (_, completeCallback) {
+                    form = $('<form style="display:none;"></form>');
+                    form.attr('accept-charset', options.formAcceptCharset);
+                    // javascript:false as initial iframe src
+                    // prevents warning popups on HTTPS in IE6.
+                    // IE versions below IE8 cannot set the name property of
+                    // elements that have already been added to the DOM,
+                    // so we set the name along with the iframe HTML markup:
+                    iframe = $(
+                        '<iframe src="javascript:false;" name="iframe-transport-' +
+                            (counter += 1) + '"></iframe>'
+                    ).bind('load', function () {
+                        var fileInputClones,
+                            paramNames = $.isArray(options.paramName) ?
+                                    options.paramName : [options.paramName];
+                        iframe
+                            .unbind('load')
+                            .bind('load', function () {
+                                var response;
+                                // Wrap in a try/catch block to catch exceptions thrown
+                                // when trying to access cross-domain iframe contents:
+                                try {
+                                    response = iframe.contents();
+                                    // Google Chrome and Firefox do not throw an
+                                    // exception when calling iframe.contents() on
+                                    // cross-domain requests, so we unify the response:
+                                    if (!response.length || !response[0].firstChild) {
+                                        throw new Error();
+                                    }
+                                } catch (e) {
+                                    response = undefined;
+                                }
+                                // The complete callback returns the
+                                // iframe content document as response object:
+                                completeCallback(
+                                    200,
+                                    'success',
+                                    {'iframe': response}
+                                );
+                                // Fix for IE endless progress bar activity bug
+                                // (happens on form submits to iframe targets):
+                                $('<iframe src="javascript:false;"></iframe>')
+                                    .appendTo(form);
+                                form.remove();
+                            });
+                        form
+                            .prop('target', iframe.prop('name'))
+                            .prop('action', options.url)
+                            .prop('method', options.type);
+                        if (options.formData) {
+                            $.each(options.formData, function (index, field) {
+                                $('<input type="hidden"/>')
+                                    .prop('name', field.name)
+                                    .val(field.value)
+                                    .appendTo(form);
+                            });
+                        }
+                        if (options.fileInput && options.fileInput.length &&
+                                options.type === 'POST') {
+                            fileInputClones = options.fileInput.clone();
+                            // Insert a clone for each file input field:
+                            options.fileInput.after(function (index) {
+                                return fileInputClones[index];
+                            });
+                            if (options.paramName) {
+                                options.fileInput.each(function (index) {
+                                    $(this).prop(
+                                        'name',
+                                        paramNames[index] || options.paramName
+                                    );
+                                });
+                            }
+                            // Appending the file input fields to the hidden form
+                            // removes them from their original location:
+                            form
+                                .append(options.fileInput)
+                                .prop('enctype', 'multipart/form-data')
+                                // enctype must be set as encoding for IE:
+                                .prop('encoding', 'multipart/form-data');
+                        }
+                        form.submit();
+                        // Insert the file input fields at their original location
+                        // by replacing the clones with the originals:
+                        if (fileInputClones && fileInputClones.length) {
+                            options.fileInput.each(function (index, input) {
+                                var clone = $(fileInputClones[index]);
+                                $(input).prop('name', clone.prop('name'));
+                                clone.replaceWith(input);
+                            });
+                        }
+                    });
+                    form.append(iframe).appendTo(document.body);
+                },
+                abort: function () {
+                    if (iframe) {
+                        // javascript:false as iframe src aborts the request
+                        // and prevents warning popups on HTTPS in IE6.
+                        // concat is used to avoid the "Script URL" JSLint error:
+                        iframe
+                            .unbind('load')
+                            .prop('src', 'javascript'.concat(':false;'));
+                    }
+                    if (form) {
+                        form.remove();
+                    }
+                }
+            };
+        }
+    });
+
+    // The iframe transport returns the iframe content document as response.
+    // The following adds converters from iframe to text, json, html, and script:
+    $.ajaxSetup({
+        converters: {
+            'iframe text': function (iframe) {
+                return $(iframe[0].body).text();
+            },
+            'iframe json': function (iframe) {
+                return $.parseJSON($(iframe[0].body).text());
+            },
+            'iframe html': function (iframe) {
+                return $(iframe[0].body).html();
+            },
+            'iframe script': function (iframe) {
+                return $.globalEval($(iframe[0].body).text());
+            }
+        }
+    });
+
+}));
+function PunkAveFileUploader(options)
+{
+  var self = this,
+    uploadUrl = options.uploadUrl,
+    viewUrl = options.viewUrl,
+    $el = $(options.el),
+    uploaderTemplate = _.template($.trim($('#file-uploader-template').html()));
+  $el.html(uploaderTemplate({}));
+
+  var fileTemplate = _.template($.trim($('#file-uploader-file-template').html())),
+    editor = $el.find('[data-files="1"]'),
+    thumbnails = $el.find('[data-thumbnails="1"]');
+  
+  self.uploading = false;
+  
+  self.errorCallback = 'errorCallback' in options ? options.errorCallback : function( info ) { if (window.console && console.log) { console.log(info) } },
+
+  self.addExistingFiles = function(files)
+  {
+    _.each(files, function(file) {
+      appendEditableImage({
+        // cmsMediaUrl is a global variable set by the underscoreTemplates partial of MediaItems.html.twig
+        'thumbnail_url': viewUrl + '/thumbnails/' + file,
+        'url': viewUrl + '/originals/' + file,
+        'name': file
+        });
+    });
+  };
+
+  // Delay form submission until upload is complete.
+  // Note that you are welcome to examine the
+  // uploading property yourself if this isn't
+  // quite right for you
+  self.delaySubmitWhileUploading = function(sel)
+  {
+    $(sel).submit(function(e) {
+        if (!self.uploading)
+        {
+            return true;
+        }
+        function attempt()
+        {
+            if (self.uploading)
+            {
+                setTimeout(attempt, 100);
+            }
+            else
+            {
+                $(sel).submit();
+            }
+        }
+        attempt();
+        return false;
+    });
+  }
+
+  if (options.blockFormWhileUploading)
+  {
+    self.blockFormWhileUploading(options.blockFormWhileUploading);
+  }
+
+  if (options.existingFiles)
+  {
+    self.addExistingFiles(options.existingFiles);
+  }
+
+  editor.fileupload({
+    dataType: 'json',
+    url: uploadUrl,
+    dropZone: $el.find('[data-dropzone="1"]'),
+    done: function (e, data) {
+      if (data)
+      {
+        _.each(data.result, function(item) {
+          appendEditableImage(item);
+        });
+      }
+    },
+    start: function (e) {
+      $el.find('[data-spinner="1"]').show();
+      self.uploading = true;
+    },
+    stop: function (e) {
+      $el.find('[data-spinner="1"]').hide();
+      self.uploading = false;
+    }
+  });
+
+  // Expects thumbnail_url, url, and name properties. thumbnail_url can be undefined if
+  // url does not end in gif, jpg, jpeg or png. This is designed to work with the
+  // result returned by the UploadHandler class on the PHP side
+  function appendEditableImage(info)
+  {
+    if (info.error)
+    {
+      self.errorCallback(info);
+      return;
+    }
+    var li = $(fileTemplate(info));
+    li.find('[data-action="delete"]').click(function(event) {
+      var file = $(this).closest('[data-name]');
+      var name = file.attr('data-name');
+      $.ajax({
+        type: 'delete',
+        url: setQueryParameter(uploadUrl, 'file', name),
+        success: function() {
+          file.remove();
+        },
+        dataType: 'json'
+      });
+      return false;
+    });
+
+    thumbnails.append(li);
+  }
+
+  function setQueryParameter(url, param, paramVal)
+  {
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1]; 
+    var temp = "";
+    if (additionalURL)
+    {
+        var tempArray = additionalURL.split("&");
+        var i;
+        for (i = 0; i < tempArray.length; i++)
+        {
+            if (tempArray[i].split('=')[0] != param )
+            {
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+    var newTxt = temp + "" + param + "=" + encodeURIComponent(paramVal);
+    var finalURL = baseURL + "?" + newAdditionalURL + newTxt;
+    return finalURL;
+  }
+}
+
+
