c9bb85a871481816340f55041903c067b8b5f96d
manuroot <mroot72000@yahoo.fr>
2014-02-18 16:56:39 +0100

    update work v1

--- clearinstall_dev.sh
+++ clearinstall_dev.sh
@@ -2,7 +2,7 @@
 php app/console cache:clear --env=dev --no-debug
 php app/console assets:install web --env=dev
 php app/console assetic:dump --env=dev  
-chown -R www-data:manuroot ../Symfony_changements
-chown -R manuroot:manuroot ../Symfony_changements/src/*
-chown -R manuroot:manuroot ../Symfony_changements/web/*
-chown -R www-data:manuroot ../Symfony_changements/web/uploads
+chown -R www-data:manuroot .
+chown -R manuroot:manuroot src/*
+chown -R manuroot:manuroot web/*
+chown -R www-data:manuroot web/uploads
