5b3c9308957db30c59e92fee32ea0bf3965b79c1
manuroot <mroot72000@yahoo.fr>
2014-06-29 23:06:23 +0200

    ajout dans sonataadmin

---
+++ src/Application/ChangementsBundle/Admin/ChangementsContactAdmin.php
@@ -0,0 +1,139 @@
+<?php
+
+// src/Tutorial/BlogBundle/Admin/PostAdmin.php
+
+namespace Application\ChangementsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Knp\Menu\ItemInterface as MenuItemInterface;
+use Application\ChangementsBundle\Entity\ChangementsContact;
+
+class ChangementsContactAdmin extends Admin {
+
+  protected $baseRouteName = 'contacts';
+
+  public function getTemplate($name)
+ 
+{
+      return parent::getTemplate($name);
+
+   
+    }
+
+ /* public function getTemplate($name)
+ 
+{
+
+switch ($name) {
+ 
+case 'edit':
+ 
+return 'ApplicationRelationsBundle::admin-edit.html.twig';
+ 
+break; 
+ 
+default:
+ 
+return parent::getTemplate($name);
+ 
+break;
+ 
+}
+ 
+}*/
+/* Show
+*/
+    /**
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     *
+     * @return void
+     */
+    protected function configureshowField(ShowMapper $showMapper) {
+        $showMapper
+     //  ->with('Détail de l\'enregistrement')
+        // ->add('id')
+                ->add('nomUser')
+                ->add('infos')
+                ->add('telephone')
+                ->add('email')
+            //    ->add('description')
+          //      ->end()
+ ;
+                  
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     *
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper) {
+        $formMapper
+                ->with('General')
+              //    ->add('id')
+                     ->add('nomUser')
+                ->add('infos')
+                ->add('telephone')
+                ->add('email')
+                //page edit supplément
+                ->setHelps(array(
+                    'nomUser' => 'Nom du user',
+                    'keywords' => 'Set the keywords of a web page',
+                ))
+             /*   ->add('image', 'sonata_type_model_list', array('required' => false),
+                   array('link_parameters'=>array('context'=>'default',
+                   'provider'=>'sonata.media.provider.image')))*/
+                ->end()
+        ;
+        //->add('enabled', null, array('required' => false))
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper
+          ->add('id')
+                   ->add('nomUser')
+                ->add('infos')
+                ->add('telephone')
+                ->add('email')
+              // ->addIdentifier('nomprojet')
+              
+                ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'view' => array(),
+                        'edit' => array(),
+                        'delete' => array(),
+                    )
+                ))
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     *
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper
+                    ->add('nomUser')
+                ->add('infos')
+                ->add('telephone')
+                ->add('email')
+
+        //  ->add('tags', null, array('field_options' => array('expanded' => true, 'multiple' => true)))
+        ;
+    }
+/*
+public function getEditTemplate()
+{
+    return 'CertificatsBundle:CertificatsProjet:base_edit.html.twig';
+}*/
+}
---
+++ src/Application/ChangementsBundle/Controller/ChangementsContactAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Application\ChangementsBundle\Controller;
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ChangementsContactAdminController extends Controller
+{
+ 
+}
